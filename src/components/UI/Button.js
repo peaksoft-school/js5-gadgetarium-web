2cb6eec8467092e3a9aec658e7f405b70b352764
damirakamchybekovna <103958077+damirakamchybekovna@users.noreply.github.com>
2022-07-14 20:11:12 +0300

    ui/button

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -1,88 +1,51 @@
 import { styled } from '@mui/material'
 import MuiButton from '@mui/material/Button'
 
-import { ReactComponent as BusketIcon } from '../../assets/icons/busket.svg'
-
-function Button(props) {
+function Button({ children, onClick, variant, ...props }) {
    return (
-      <ButtonStyled
-         fullWidth={props.fullWidth}
-         onClick={props.onClick}
-         {...props}
-      >
-         {props.basketIcon && <BasketIconStyled {...props} />}
-
-         {props.children}
+      <ButtonStyled onClick={onClick} variant={variant} {...props}>
+         {children}
       </ButtonStyled>
    )
 }
 export default Button
 
 const ButtonStyled = styled(MuiButton)`
+   width: ${(props) => props.width && props.width};
    text-transform: none;
-   display: flex;
-   justify-content: space-between;
-   background: none;
-   weight: 500;
+   width: ${(props) => props.width};
+   font-family: ;
+   font-weight: 500;
    font-size: 14px;
    line-height: 16.95px;
    border-radius: 4px;
    color: #ffffff;
-   padding: 12px 19px 12px 19px;
-   text-align: center;
-   &:hover {
-      background: #e313bf;
-      color: #ffffff;
-   }
-   &:active {
-      background: #c90ea9;
-      color: #ffffff;
-   }
-   &::after {
-      content: url(BusketIcon);
-   }
-
+   padding: 12px 19px;
    width: ${(props) => (props.width ? props.width : '')};
    height: ${(props) => (props.height ? props.height : '')};
-   color: ${(props) => (props.Color ? props.Color : '')};
-   background: ${(props) => (props.background ? props.background : ' #E20FBE')};
-   border: ${(props) => (props.border ? props.border : '')};
-
-   &:hover {
-      background: ${(props) => props.background || '#CB11AB'};
-   }
-   &:active {
-      background: ${({ props }) => props || '#E20FBE'};
-   }
 
    &.MuiButton-contained {
-      background-color: #2fc509;
+      background: ${({ bgcolor }) => bgcolor || '#E20FBE'};
       font-size: 14px;
       border: none;
       color: #ffffff;
       &:hover {
-         background: #2fc509;
+         background: ${({ bgcolor }) => (bgcolor ? '#2fc509' : '#CB11AB')};
+         color: #ffffff;
       }
       &:active {
-         background: #2fc509;
+         background: ${({ bgcolor }) => (bgcolor ? '#2fc509' : '#E313BF')};
       }
    }
    &.MuiButton-outlined {
-      background: none;
-      color: #c90ea9;
-      border: 1px solid #c90ea9;
+      color: ${(props) => (props.bgcolor ? 'gray' : '#E313BF')};
+      border-color: ${(props) => props.bgcolor || '#E313BF'};
    }
    &:hover {
-      background: ${(props) => props.background || '#CB11AB'};
+      background: ${(props) => props.bgcolor || '#CB11AB'};
       color: #ffffff;
    }
    &:active {
-      background: ${({ props }) => props || '#E20FBE'};
-      color: #ffffff;
+      background: ${({ bgcolor }) => (bgcolor ? '#969696' : '#E313BF')};
    }
-`
-const BasketIconStyled = styled(BusketIcon)`
-   width: 20.72px;
-   height: 21px;
-   margin-right: 6px;
 `
