172f7fc951f2673342aca067c9a9f2a7d5a7e25f
wingmeter <nursb189@gmail.com>
2022-07-14 16:25:38 +0600

    add error snackbar

--- src/components/UI/snackbars/SuccessSnackbar.js
+++ src/components/UI/snackbars/SuccessSnackbar.js
@@ -1,18 +1,17 @@
 import { IconButton } from '@mui/material'
 import Slide from '@mui/material/Slide'
 import MuiSnackbar from '@mui/material/Snackbar'
-import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
-import { ReactComponent as Close } from '../../assets/icons/closeIcon.svg'
+import { ReactComponent as Close } from '../../../assets/icons/closeIcon.svg'
 
 const SlideTransition = (props) => {
    return <Slide {...props} direction="left" />
 }
 
-const Snackbar = ({ handleClose, open, message, link, navigation }) => {
+const SuccessSnackbar = ({ handleClose, open, message, link, navigation }) => {
    return (
-      <MuiSnackbar
+      <StyledSnackbarContainer
          open={open}
          autoHideDuration={3000}
          onClose={handleClose}
@@ -23,7 +22,7 @@ const Snackbar = ({ handleClose, open, message, link, navigation }) => {
          message={<SnackbarParagrapgh>{message}</SnackbarParagrapgh>}
          action={[
             <>
-               <SnackbarNavigations to={navigation}>{link}</SnackbarNavigations>
+               <SnackbarNavigations to={link}>{navigation}</SnackbarNavigations>
                <IconButton
                   key="close"
                   aria-label="Close"
@@ -38,7 +37,7 @@ const Snackbar = ({ handleClose, open, message, link, navigation }) => {
    )
 }
 
-export default Snackbar
+export default SuccessSnackbar
 
 const SnackbarParagrapgh = styled.p`
    padding: 20px 15px;
@@ -47,11 +46,17 @@ const SnackbarParagrapgh = styled.p`
    font-weight: 400;
 `
 
-const SnackbarNavigations = styled(Link)`
+const SnackbarNavigations = styled.a`
    padding: 21px 15px;
    color: #3cde14;
    font-size: 18px;
    font-weight: 700;
    cursor: pointer;
    text-decoration: none;
+`
+
+const StyledSnackbarContainer = styled(MuiSnackbar)`
+   & .css-1eqdgzv-MuiPaper-root-MuiSnackbarContent-root {
+      background: #202027;
+   }
 `
