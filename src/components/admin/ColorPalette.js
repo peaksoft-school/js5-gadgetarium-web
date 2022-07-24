f861d3ba1098833fba15fa3bfb0aa3acaaea63a2
zamirbekovsamagan <zamirbekovsamagan1@gmail.com>
2022-07-25 01:03:53 +0300

    fixed color picker

--- src/components/admin/ColorPalette.js
+++ src/components/admin/ColorPalette.js
@@ -1,16 +1,16 @@
 import React, { useState, useRef } from 'react'
 
-import { InputAdornment, Paper, Popover, styled } from '@mui/material'
+import { InputAdornment, Popover } from '@mui/material'
+import { ChromePicker } from 'react-color'
 
 import { ReactComponent as ColorIcon } from '../../assets/icons/color-icon.svg'
-import colorList from '../../utils/constants/colors'
 import Input from '../UI/inputs/Input'
 
-function ColorPalette({ colorHandler }) {
+function ColorPalette({ getColor }) {
    const [color, setColor] = useState('')
    const colorChangeHandler = (col) => {
       setColor(col)
-      colorHandler(col)
+      getColor(col)
    }
    const [anchorEl, setAnchorEl] = useState(null)
    const divRef = useRef()
@@ -26,79 +26,39 @@ function ColorPalette({ colorHandler }) {
 
    return (
       <>
-         <Input
-            ref={divRef}
-            height="35px"
-            value={color}
-            endAdornment={
-               <InputAdornment position="end">
-                  <ColorIcon onClick={() => handleClick()} />
-               </InputAdornment>
-            }
-            variant="outlined"
-         />
+         <div ref={divRef}>
+            <Input
+               height="35px"
+               value={color}
+               placeholder="Основной цвет"
+               endAdornment={
+                  <InputAdornment position="end">
+                     <ColorIcon onClick={() => handleClick()} />
+                  </InputAdornment>
+               }
+            />
+         </div>
          <Popover
             open={open}
             anchorEl={anchorEl}
             onClose={() => handleClose()}
             anchorOrigin={{
                vertical: 'bottom',
-               horizontal: 'left',
+               horizontal: 'right',
             }}
             transformOrigin={{
                vertical: 'top',
                horizontal: 'right',
             }}
          >
-            <Container>
-               {colorList.map((item, index) => {
-                  return (
-                     <Column key={item[index]}>
-                        {item.map((el, ind) => {
-                           return (
-                              <Color
-                                 key={el[ind]}
-                                 background={el}
-                                 border={color === el ? 'red' : el}
-                                 onClick={() => colorChangeHandler(el)}
-                              />
-                           )
-                        })}
-                     </Column>
-                  )
-               })}
-            </Container>
+            <ChromePicker
+               color={color}
+               onChangeComplete={(сolor) => colorChangeHandler(сolor.hex)}
+               disableAlpha
+            />
          </Popover>
       </>
    )
 }
 
 export default ColorPalette
-
-const Container = styled(Paper)`
-   display: flex;
-   width: 396px;
-   height: 268px;
-   padding: 2px 6px;
-   border-radius: 4px;
-   box-shadow: 0px 4px 16px rgba(0, 0, 0, 0.1);
-   & * {
-      box-sizing: border-box;
-   }
-`
-
-const Column = styled('div')`
-   width: 32px;
-   margin: 2px;
-`
-
-const Color = styled('div')`
-   height: 28px;
-   width: 28px;
-   margin: 4px 0 4px 0;
-   background-color: ${(props) => props.background};
-   border: ${(props) => `2px solid ${props.border}`};
-   &:hover {
-      transform: scale(1.1);
-   }
-`
