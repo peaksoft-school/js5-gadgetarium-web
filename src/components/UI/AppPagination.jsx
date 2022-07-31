87467052fe43cafc7bd3cfc7e49b220d00576a29
Aman <aman.saamaibekov@gmail.com>
2022-07-31 21:59:54 +0600

    pagination(fixed)

--- src/components/UI/AppPagination.jsx
+++ src/components/UI/AppPagination.jsx
@@ -1,20 +1,24 @@
 import React from 'react'
 
-import { Box } from '@mui/material'
+import { Box, styled } from '@mui/material'
 import Pagination from '@mui/material/Pagination'
 
-const AppPagination = () => {
+const AppPagination = ({ page, totalPage, onChange }) => {
    return (
       <Box justifyContent="center" alignItems="center" display="flex">
-         <Pagination
+         <PaginationStyled
             count={10}
+            page={page}
+            totalPage={totalPage}
+            onChange={onChange}
             color="secondary"
-            sx={{
-               margin: '20px 4px 20px 4px',
-            }}
          />
       </Box>
    )
 }
+
+const PaginationStyled = styled(Pagination)`
+   margin: 20px 4px 20px 4px;
+`
 
 export default AppPagination
