1a89ff77848e89f2a062988feb34b078d540cd39
umutabdyldaeva <umut.abdyldaeva91@gmail.com>
2022-08-07 21:47:39 +0600

    feature/cart/hover/popup

---
+++ src/components/Mainpage.jsx
@@ -0,0 +1,3 @@
+const Mainpage = () => {}
+
+export default Mainpage
---
+++ src/components/UI/popupMenus/CartPopup.jsx
@@ -0,0 +1,155 @@
+import { useState } from 'react'
+
+import styled from '@emotion/styled'
+import Menu from '@mui/material/Menu'
+import MenuItem from '@mui/material/MenuItem'
+
+import { ReactComponent as IconDelete } from '../../../assets/icons/Vertor-delete.svg'
+import Button from '../Button'
+
+export default function AccountMenu({ cart }) {
+   const [anchorEl, setAnchorEl] = useState(null)
+   const open = Boolean(anchorEl)
+
+   // const handleClick = (event) => {
+   //    setAnchorEl(event.currentTarget)
+   // }
+
+   const handleClose = () => {
+      setAnchorEl(null)
+   }
+
+   const handlerClick = () => {}
+
+   const handleChange = () => {}
+
+   const totalPrice = cart.reduce((prev, current) => prev + current.price, 0)
+   return (
+      <div>
+         <Menu
+            anchorEl={anchorEl}
+            id="account-menu"
+            open={open}
+            onClose={handleClose}
+            PaperProps={{
+               elevation: 0,
+               sx: {
+                  overflow: 'visible',
+                  filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                  mt: 1.5,
+                  '& .MuiAvatar-root': {
+                     width: 32,
+                     height: 32,
+                     ml: -0.5,
+                     mr: 1,
+                  },
+                  '&:before': {
+                     content: '""',
+                     display: 'block',
+                     position: 'absolute',
+                     top: 0,
+                     right: 14,
+                     width: 10,
+                     height: 10,
+                     bgcolor: 'background.paper',
+                     transform: 'translateY(-50%) rotate(45deg)',
+                     zIndex: 0,
+                  },
+               },
+            }}
+            transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+            anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+         >
+            <MenuBlock>
+               <Div>
+                  {cart.map((el) => (
+                     <StyledMenuItem key={el.id} img={el.img}>
+                        <ContainerDates>
+                           <div>{el.title}</div>
+                           <div>{el.color}</div>
+                        </ContainerDates>
+                        <ContainerPrice>{el.price}</ContainerPrice>
+                        <img src={el.img} alt="" />
+                        <ContainerIcon onClick={handlerClick}>
+                           <IconDelete />
+                        </ContainerIcon>
+                     </StyledMenuItem>
+                  ))}
+               </Div>
+               <ContainerButton>
+                  <Button
+                     variant="contained"
+                     width="172px"
+                     height="43px"
+                     onClick={handleChange}
+                  >
+                     Оформить заказ
+                  </Button>
+                  <p>Итого {totalPrice}с </p>
+               </ContainerButton>
+            </MenuBlock>
+         </Menu>
+      </div>
+   )
+}
+
+const MenuBlock = styled('div')`
+   width: 510px;
+   height: 277px;
+   padding-right: 5px;
+   display: flex;
+   flex-direction: column;
+`
+
+const StyledMenuItem = styled(MenuItem)`
+   width: 456px;
+   height: 89px;
+   border-bottom: 1px solid rgba(133, 143, 164, 0.15);
+`
+
+const Div = styled('div')`
+   padding: 20px 22px 20px 24px;
+   width: 100%;
+   display: flex;
+   align-items: center;
+   flex-wrap: wrap;
+   justify-content: space-around;
+
+   overflow: auto;
+   ::-webkit-scrollbar {
+      width: 5px;
+   }
+   /* Track */
+   ::-webkit-scrollbar-track {
+      background: #f1f1f1;
+   }
+
+   /* Handle */
+   ::-webkit-scrollbar-thumb {
+      background: #888;
+      border-radius: 10px;
+   }
+   /* Handle on hover */
+   ::-webkit-scrollbar-thumb:hover {
+      background: #555;
+   }
+`
+const ContainerButton = styled(Div)`
+   height: 140px;
+`
+const ContainerDates = styled('div')`
+   width: 255px;
+   height: 48px;
+   margin-left: 72px;
+`
+
+const ContainerPrice = styled('div')`
+   width: 72px;
+   height: 17px;
+   margin-right: 25px;
+   margin-bottom: 30px;
+`
+const ContainerIcon = styled('p')`
+   margin-bottom: 30px;
+   margin-right: -23px;
+`
