6a28a1b36726de3baa8774d97ab8555499eb4f83
wingmeter <nursb189@gmail.com>
2022-09-01 14:40:55 +0600

    add new features

--- src/layout/elements/Header.jsx
+++ src/layout/elements/Header.jsx
@@ -9,9 +9,10 @@ import favorites from '../../assets/icons/favorites.svg'
 import instagram from '../../assets/icons/instagram.svg'
 import logo from '../../assets/icons/logo.svg'
 import menu from '../../assets/icons/menu.svg'
-import profileLogo from '../../assets/icons/profile-icon.svg'
 import cart from '../../assets/icons/shopping-cart.svg'
 import whatsapp from '../../assets/icons/whatsapp.svg'
+import UserProfile from '../../components/client/UserProfile'
+import MuiBadge from '../../components/UI/MuiBadge'
 
 const Header = () => {
    return (
@@ -38,51 +39,24 @@ const Header = () => {
                      <StyledNavLink to="delivery"> Доставка </StyledNavLink>
                   </li>
                   <li>
-                     <StyledNavLink to="fag"> FAG </StyledNavLink>
+                     <StyledNavLink to="faq"> FAQ </StyledNavLink>
                   </li>
                   <li>
                      <StyledNavLink to="contacts"> Контакты </StyledNavLink>{' '}
                   </li>
                </MenuNavBar>
-               {/* <Menu>
-                  <BackgroundButton>
-                     <StyledNavLink
-                        to="/"
-                        className={({ isActive }) =>
-                           isActive ? 'active' : 'default'
-                        }
-                     >
-                        Главная
-                     </StyledNavLink>
-                  </BackgroundButton>
-                  <BackgroundButton>
-                     <NavLink to="about"> О магазине </NavLink>
-                  </BackgroundButton>
-                  <BackgroundButton>
-                     <NavLink to="delivery"> Доставка </NavLink>
-                  </BackgroundButton>
-                  <BackgroundButton>
-                     <NavLink to="fag"> FAG </NavLink>
-                  </BackgroundButton>
-                  <BackgroundButton>
-                     <NavLink to="contacts"> Контакты </NavLink>
-                  </BackgroundButton>
-               </Menu> */}
-               <UserProfile>
-                  <p> +996 (400) 00-00-00 </p>
-                  <NavLink to="#">
-                     <ProfileLogo src={profileLogo} />
-                  </NavLink>
-               </UserProfile>
+               <UserProfile />
             </UpperRow>
             <MiddleLine />
          </HeaderTop>
          <NavBar>
             <BottomRow>
-               <Catalog>
-                  <img src={menu} alt="#" />
-                  Каталог
-               </Catalog>
+               <StyledLink to="catalog">
+                  <Catalog>
+                     <img src={menu} alt="#" />
+                     Каталог
+                  </Catalog>
+               </StyledLink>
                <VerticalLine />
                <SearchInput
                   padding="2em"
@@ -101,13 +75,19 @@ const Header = () => {
                </SocialLinks>
                <UserTools>
                   <NavLink to="comparison">
-                     <ImageBox src={compare} />
+                     <MuiBadge>
+                        <ImageBox src={compare} />
+                     </MuiBadge>
                   </NavLink>
                   <NavLink to="favourites">
-                     <ImageBox src={favorites} />
+                     <MuiBadge>
+                        <ImageBox src={favorites} />
+                     </MuiBadge>
                   </NavLink>
                   <NavLink to="cart">
-                     <ImageBox src={cart} />
+                     <MuiBadge>
+                        <ImageBox src={cart} />
+                     </MuiBadge>
                   </NavLink>
                </UserTools>
             </BottomRow>
@@ -123,7 +103,17 @@ const HeaderTop = styled('header')`
    margin-left: auto;
    margin-right: auto;
    height: 82px;
-   padding: 18px 0;
+   padding: 18px 100px;
+
+   @media (max-width: 1536px) {
+      max-width: 1440px;
+      padding: 18px 30px;
+   }
+
+   @media (max-width: 1240px) {
+      max-width: 1200px;
+      padding: 18px 30px;
+   }
 `
 const HeaderContainer = styled('div')`
    width: 100%;
@@ -135,17 +125,21 @@ const UpperRow = styled('div')`
    align-items: center;
    padding: 0 0 20px 0;
 `
-
 const NavBar = styled('nav')`
    max-width: 1920px;
    margin-left: auto;
    margin-right: auto;
    height: 90px;
-   padding: 18px 0;
+   padding: 18px 100px;
    background: #1a1a25;
    position: sticky;
    top: 0;
    z-index: 99;
+
+   @media (max-width: 1536px) {
+      max-width: 1440px;
+      padding: 18px 30px;
+   }
 `
 const BottomRow = styled('div')`
    display: flex;
@@ -173,6 +167,10 @@ const SearchInput = styled('input')`
    ::placeholder {
       color: white;
    }
+
+   @media (max-width: 1536px) {
+      width: 600px;
+   }
 `
 const SocialLinks = styled('div')`
    width: 115px;
@@ -188,7 +186,6 @@ const Logo = styled('img')`
    height: 39px;
    cursor: pointer;
 `
-
 const StyledNavLink = styled(NavLink)`
    width: 100%;
    display: flex;
@@ -201,6 +198,10 @@ const StyledNavLink = styled(NavLink)`
       transition: 0.3s;
       border-radius: 4px;
       background: rgba(133, 143, 164, 0.15);
+   }
+   @media (max-width: 1536px) {
+      font-size: 14px;
+      padding: 12px 10px;
    }
 `
 const MenuNavBar = styled('ul')`
@@ -223,49 +224,6 @@ const MenuNavBar = styled('ul')`
       background: rgba(133, 143, 164, 0.15);
    }
 `
-// const BackgroundButton = styled('div')`
-//    .default {
-//       padding: 13px 15px;
-//       border-radius: 4px;
-//       display: flex;
-//       align-items: center;
-//       justify-content: center;
-//    }
-
-//    :hover {
-//       transition: 0.3s;
-//       border-radius: 4px;
-//       display: flex;
-//       align-items: center;
-//       justify-content: center;
-//       background: rgba(133, 143, 164, 0.15);
-//    }
-//    .active {
-//       padding: 13px 15px;
-//       border-radius: 4px;
-//       display: flex;
-//       align-items: center;
-//       justify-content: center;
-//       transition: 0.3s;
-//       border-radius: 4px;
-//       display: flex;
-//       align-items: center;
-//       justify-content: center;
-//       background: rgba(133, 143, 164, 0.15);
-//    }
-// `
-const UserProfile = styled('div')`
-   width: 232px;
-   height: 30px;
-   display: flex;
-   align-items: center;
-   justify-content: space-between;
-   color: #ffffff;
-`
-const ProfileLogo = styled('img')`
-   width: 24px;
-   height: 24px;
-`
 const Catalog = styled('button')`
    width: 136px;
    height: 49px;
@@ -276,6 +234,8 @@ const Catalog = styled('button')`
    align-items: center;
    justify-content: space-evenly;
    font-size: 16px;
+   cursor: pointer;
+   text-decoration: none;
 `
 const VerticalLine = styled('div')`
    width: 1.07px;
@@ -285,4 +245,9 @@ const VerticalLine = styled('div')`
 const ImageBox = styled('img')`
    width: 24px;
    height: 24px;
+`
+const StyledLink = styled(Link)`
+   text-decoration: none;
+   color: white;
+   padding: 2px;
 `
